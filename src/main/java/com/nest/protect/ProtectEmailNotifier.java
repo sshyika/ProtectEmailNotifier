161f51a4057075e137ad2b1981e7d80ad7e98b80
Serhii Shyika <sshyika@amplify.com>
2016-02-15 16:53:08 +0200

    Email notifications

--- src/main/java/com/nest/protect/ProtectEmailNotifier.java
+++ src/main/java/com/nest/protect/ProtectEmailNotifier.java
@@ -2,6 +2,7 @@ package com.nest.protect;
 
 import com.nest.protect.connector.MailConnector;
 import com.nest.protect.connector.NestConnector;
+import com.nest.protect.model.Level;
 import com.nest.protect.model.Model;
 import com.nest.protect.model.Protect;
 import org.slf4j.Logger;
@@ -11,9 +12,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import java.util.function.Function;
-import java.util.logging.Filter;
 import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 public class ProtectEmailNotifier {
     private static final Logger LOG = LoggerFactory.getLogger(ProtectEmailNotifier.class);
@@ -26,11 +25,11 @@ public class ProtectEmailNotifier {
         final MailConnector mail = MailConnector.build(config);
 
         nest.addHomeStateListener(newModel -> {
-            List<Protect> updated = findUpdated(model, newModel);
-            LOG.debug("Found {} updated protect devices", updated.size());
+            List<Change> changes = findUpdated(model, newModel);
+            LOG.debug("Found {} updated protect devices", changes.size());
 
-            for (Protect protect : updated) {
-                mail.send(config.getUserMail(), "test", "test");
+            for (Change change : changes) {
+                mail.send(config.getUserMail(), "Nest Protect`s status updated", change.getMessage());
             }
 
             model = newModel;
@@ -38,21 +37,56 @@ public class ProtectEmailNotifier {
     }
 
 
-    private List<Protect> findUpdated(Model oldModel, Model newModel) {
-        List<Protect> updated = new ArrayList<>();
+    private List<Change> findUpdated(Model oldModel, Model newModel) {
+        List<Change> updated = new ArrayList<>();
         if (oldModel != null) {
             Map<String, Protect> oldDevices = oldModel.getStructures().stream()
                     .flatMap(s -> s.getProtectDevices().values().stream())
                     .collect(Collectors.toMap(Protect::getId, Function.identity()));
+
             newModel.getStructures().stream()
                     .flatMap(s -> s.getProtectDevices().values().stream())
-                    .filter(p -> {
-                        Protect old = oldDevices.get(p.getId());
-                        return (old != null && (old.getSmokeAlarm() != p.getSmokeAlarm() || old.getCoAlarm() != p.getCoAlarm()));
-                    })
+                    .map(p -> new Change(p, oldDevices.get(p.getId())))
+                    .filter(c -> c.was != null && (c.was.getSmokeAlarm() != c.now.getSmokeAlarm() || c.was.getCoAlarm() != c.now.getCoAlarm()))
                     .forEach(updated::add);
         }
         return updated;
+    }
+
+
+    private class Change {
+        Protect was;
+        Protect now;
+
+        Change(Protect now, Protect was) {
+            this.now = now;
+            this.was = was;
+        }
+
+        String getMessage() {
+            return String.format("Your protect device located in the %s of your %s detected that:\n%s",
+                    now.getWhere().getName(), now.getWhere().getStructure().getName(), getLevels());
+        }
+
+        String getLevels() {
+            String result = "";
+
+            String coLevel = (now.getCoAlarm() == Level.OK ? "normal" : (now.getCoAlarm() == Level.WARNING ? "high" : "critical"));
+            String smokeLevel = (now.getSmokeAlarm() == Level.OK ? "normal" : (now.getSmokeAlarm() == Level.WARNING ? "high" : "critical"));
+
+            if (was.getCoAlarm() != now.getCoAlarm()) {
+                result += "CO level now is " + coLevel + ".\n";
+            } else {
+                result += "CO level still " + coLevel + ".\n";
+            }
+            if (was.getSmokeAlarm() != now.getSmokeAlarm()) {
+                result += "Smoke level now is " + smokeLevel + ".\n";
+            } else {
+                result += "Smoke level still " + smokeLevel + ".\n";
+            }
+            return result;
+        }
+
     }
 
 }
